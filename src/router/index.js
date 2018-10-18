db46a3217f720fa35bc05a7d29b49bfd3836eda4
crazytuzi <957824770@qq.com>
2018-10-18 15:47:58 +0800

    挖坑

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,101 @@
+//引入vue
 import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import NavMenu from '@/views/head/NavMenu'
+//获取参数
+import  getQueryString from './getQueryString';
+//引入路由组件
+import Router from 'vue-router';
 
-Vue.use(Router)
+import cookie from '../static/js/cookie';
 
-export default new Router({
+//注册路由
+Vue.use(Router);
+//引入路由需要的组件
+
+
+//公共部分
+import app from '../views/app/app';
+
+//全局状态控制引入
+import store from '../store/store'
+
+//异步加载首页
+// var home = function(resolve) {
+//   require.ensure(['../views/home/home'], () => {
+//     resolve(require('../views/home/home'))
+//   }, 'home')
+// };
+
+import Home from '../views/home/home'
+import NavMenu from '../views/head/NavMenu'
+import footer from '../views/footer/footer'
+
+
+
+//配置路由
+let router = new Router({
   routes: [
     {
-      path: '/',
-      name: 'NavMenu',
-      component: NavMenu
+      path: '/app',
+      component: app,
+      children: [
+        {
+          path: 'home',
+          name: 'home',
+          component: {
+            head: NavMenu,
+            content: NavMenu,
+            footer: footer,
+          },
+          meta: {
+            title: "首页",
+            need_log: false
+          }
+        }
+      ]
     }
   ]
-})
+});
+
+//进行路由判断
+router.beforeEach((to, from, next) => {
+  let nextPath = cookie.getCookie('nextPath');
+  console.log(nextPath);
+  console.log("to",to);
+  console.log("from",from);
+    if (to !== undefined) {
+      if (to.meta.need_log) {
+        console.log(to.meta.need_log);
+        if (!store.state.userInfo.token) {
+          next({
+            path: '/app/home',
+          });
+        } else {
+          next();
+        }
+      } else {
+        if (to.path === '/') {
+          next({
+            path: '/app/home',
+          });
+        } else {
+          next();
+        }
+      }
+    } else {
+      if (to.path === '/') {
+        next({
+          path: '/app/home',
+        });
+      } else {
+        next();
+      }
+    }
+});
+
+//修改网页标题
+router.afterEach((to, from, next) => {
+  document.title = to.matched[to.matched.length - 1].meta.title;
+});
+
+//抛出路由
+export default router;
--- src/views/head/NavMenu.vue
+++ src/views/head/NavMenu.vue
@@ -1,18 +1,13 @@
 <template>
   <div>
+    hhhhh
     <el-menu :default-active="defaultIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-      <el-menu-item v-for = "tab in menuList" :index="tab.index"><i :class="tab.icon"></i>{{tab.name}}</el-menu-item>
+      <el-menu-item v-for = "menu in menuList" :index="menu.index"><i :class="menu.icon"></i>{{menu.name}}</el-menu-item>
     </el-menu>
-    <component :is="activeComponent"></component>
   </div>
 </template>
 <script>
-  import Home from "@/views/home/home"
-  import Problems from "@/views/problem/problems"
-  import Status from "@/views/status/status"
-  import Contests from "@/views/contest/contests"
-  import Rank from "@/views/rank/rank"
-
+  console.log("进入了");
   export default {
     data() {
       return {
@@ -21,46 +16,34 @@
             name: "Home",
             index: "1",
             icon: "el-icon-menu",
-            component: Home,
           },
           {
             name: "Problems",
             index: "2",
             icon: "el-icon-location",
-            component: Problems,
           },
           {
             name: "Status",
             index: "3",
             icon: "el-icon-location",
-            component: Status,
           },
           {
             name: "Contests",
             index: "4",
             icon: "el-icon-location",
-            component: Contests,
           },
           {
             name: "Rank",
             index: "5",
             icon: "el-icon-location",
-            component: Rank,
           }
         ],
         defaultIndex: '1',
-        activeComponent : Home,
       };
     },
-    comments:
-      {
-        Home,
-        Problems,
-        Status,
-      },
     methods: {
       handleSelect(key, keyPath) {
-        this.activeComponent = this.menuList[key-1].component;
+        console.log(key,keyPath);
       }
     }
   }
