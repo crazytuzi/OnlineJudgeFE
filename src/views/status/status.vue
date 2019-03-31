8d213a47b612b50a2c2c3e2bceb9a1a87c57eb9a
crazytuzi <957824770@qq.com>
2019-03-31 22:40:59 +0800

    Status button

--- src/views/status/status.vue
+++ src/views/status/status.vue
@@ -40,13 +40,15 @@
           <el-table-column
             prop="id"
             label="ID"
-            width="180">
+            width="75"
+            align="center">
           </el-table-column>
           <el-table-column
             label="Problem ID"
-            width="180">
-            <template slot-scope="scope" >
-              <router-link tag='a' :to="'/app/problem/' + submissions[scope.$index].problem" >
+            width="100"
+            align="center">
+            <template slot-scope="scope">
+              <router-link tag='a' :to="'/app/problem/' + submissions[scope.$index].problem">
                 {{submissions[scope.$index].problem}}
               </router-link>
             </template>
@@ -54,31 +56,57 @@
           <el-table-column
             prop="username"
             label="Username"
-            width="180"
+            width="100"
+            align="center"
           >
           </el-table-column>
           <el-table-column
             prop="result"
             label="Result"
             width="180"
+            align="center"
           >
+            <template slot-scope="scope">
+              <el-button type="primary" round size="small" v-if="submissions[scope.$index].result===Status.PENDING" style="background-color: #999;border-color: #FFFFFF">Pending</el-button>
+              <el-button type="primary" round size="small" v-else-if="submissions[scope.$index].result===Status.ACCEPTED" style="background-color: #5cb85c;border-color: #FFFFFF">Accepted</el-button>
+              <el-button type="primary" round size="small" v-else-if="submissions[scope.$index].result===Status.PRESENTATION_ERROR" style="background-color: #428bca;border-color: #FFFFFF">Presentation Error</el-button>
+              <el-button type="primary" round size="small" v-else-if="submissions[scope.$index].result===Status.WRONG_ANSWER" style="background-color: #d9534f;border-color: #FFFFFF">Wrong Answer</el-button>
+              <el-button type="primary" round size="small" v-else-if="submissions[scope.$index].result===Status.TIME_LIMIT_EXCEED" style="background-color: #f0ad4e;border-color: #FFFFFF">Time Limit Exceed</el-button>
+              <el-button type="primary" round size="small" v-else-if="submissions[scope.$index].result===Status.MEMORY_LIMIT_EXCEED" style="background-color: #f0ad4e;border-color: #FFFFFF">Memory Limit Exceed</el-button>
+              <el-button type="primary" round size="small" v-else-if="submissions[scope.$index].result===Status.OUTPUT_LIMIT_EXCEED" style="background-color: #FFFFFF;border-color: #FFFFFF">Output Limit Exceed</el-button>
+              <el-button type="primary" round size="small" v-else-if="submissions[scope.$index].result===Status.RUNTIME_ERROR" style="background-color: #d9534f;border-color: #FFFFFF">Runtime Error</el-button>
+              <el-button type="primary" round size="small" v-else-if="submissions[scope.$index].result===Status.COMPILE_ERROR" style="background-color: #d9534f;border-color: #FFFFFF">Compile Error</el-button>
+              <el-button type="primary" round size="small" v-else-if="submissions[scope.$index].result===Status.RESTRICTED_FUNCTION" style="background-color: #428bca;border-color: #FFFFFF">Restricted Function</el-button>
+              <el-button type="primary" round size="small" v-else-if="submissions[scope.$index].result===Status.SYSTEM_ERROR" style="background-color: #428bca;border-color: #FFFFFF">System Error</el-button>
+              <el-button type="primary" round size="small" v-else style="background-color: #428bca;border-color: #FFFFFF">Other</el-button>
+            </template>
           </el-table-column>
           <el-table-column
             prop="time_cost"
             label="Time"
             width="180"
+            align="center"
           >
           </el-table-column>
           <el-table-column
             prop="memory_cost"
             label="Memory"
             width="180"
+            align="center"
+          >
+          </el-table-column>
+          <el-table-column
+            prop="submit_time"
+            label="Language"
+            width="180"
+            align="center"
           >
           </el-table-column>
           <el-table-column
             prop="submit_time"
             label="Submit Time"
             width="180"
+            align="center"
           >
           </el-table-column>
         </el-table>
@@ -95,42 +123,65 @@
 
 <script>
   import {getSubmissions} from '../../api/api'
+  let Status={
+      PENDING : 0,
+      ACCEPTED : 1,
+      PRESENTATION_ERROR : 2,
+      WRONG_ANSWER : 3,
+      TIME_LIMIT_EXCEED : 4,
+      MEMORY_LIMIT_EXCEED : 5,
+      OUTPUT_LIMIT_EXCEED : 6,
+      RUNTIME_ERROR : 7,
+      COMPILE_ERROR : 8,
+      RESTRICTED_FUNCTION : 9,
+      SYSTEM_ERROR : 10,
+      COMPILE_SPECIAL_JUDGE_ERROR : 11,
+      RUN_SPECIAL_JUDGE_ERROR : 12,
+      JUDGER_ERROR : 13,
+      PARTLY_CORRECT : 14,
+      NO_ANSWERS : 15,
+      RUN_SUCCESSFULLY : 16
+    };
     export default {
         name: "Status",
         data(){
           return {
+
             results: [{
               value: '',
               label: 'Result'
             }, {
-              value: '0',
+              value: Status.PENDING,
               label: 'Pending'
             }, {
-              value: '1',
+              value: Status.ACCEPTED,
               label: 'Accepted'
             }, {
-              value: '2',
+              value: Status.PRESENTATION_ERROR,
               label: 'Presentation Error'
             }, {
-              value: '3',
+              value: Status.WRONG_ANSWER,
               label: 'Wrong Answer'
             }, {
-              value: '4',
+              value: Status.TIME_LIMIT_EXCEED,
               label: 'Time Limit Exceed'
             }, {
-              value: '5',
+              value: Status.MEMORY_LIMIT_EXCEED,
               label: 'Memory Limit Exceed'
             }, {
-              value: '6',
+              value: Status.OUTPUT_LIMIT_EXCEED,
               label: 'Output Limit Exceed'
             }, {
-              value: '7',
+              value: Status.RUNTIME_ERROR,
               label: 'Runtime Error'
             }, {
-              value: '8',
+              value: Status.COMPILE_ERROR,
               label: 'Compile Error'
             }, {
-              value: '10',
+              value: Status.RESTRICTED_FUNCTION,
+              label: 'Restricted Function'
+            },{
+              value: Status.SYSTEM_ERROR,
               label: 'System Error'
             }],
             languages: [{
@@ -153,6 +204,7 @@
             submissions: [],
             pageSize: 5,
             total: 1000,
+            Status: Status
           };
         },
         created(){
